b04b7a6b17952de0a46f9c60207f11a804590ebc
rahul-kr-soni <85615221+rahul-kr-soni@users.noreply.github.com>
2023-03-22 17:58:57 +0530

    Writing APIs to Target(sink)

---
+++ writing to target/writing_to _Source_Spark_dataframe/src/main/scala/writing_csv_file.scala
@@ -0,0 +1,43 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
+object writing_csv_file extends App {
+
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  val sparkConf = new SparkConf()
+  sparkConf.set("spark.app.name", "My first Spark_Dataframe")
+  sparkConf.set("spark.master", "local[2]")
+
+
+  val spark = SparkSession.builder()
+    //.appName("My Spark_dataframe_app_1")
+    //.master("local[2]")
+    .config(sparkConf)
+    .getOrCreate()
+
+  // reading CSv File.
+  val orderdf = spark.read
+    .format("csv")
+    .option("header", true)
+    .option("inferSchema", true)
+    .option("path", "G:\\scalalearning\\Spark_Dataframe1\\Spark_Dataframe_part1\\Data\\orders-201019-002101.csv")
+    .load
+
+  //repartioning to testing
+
+  println("orderdf has ", orderdf.rdd.getNumPartitions)
+
+  val orderRep = orderdf.repartition(4)
+  println("orderdf has ", orderRep.rdd.getNumPartitions)
+
+  orderRep.write
+    .format("csv")
+    .mode(SaveMode.Overwrite)
+    .option("path", "G:\\scalalearning\\writing_to _Source_Spark_dataframe\\output_source")
+    .save()
+
+  orderdf.printSchema()
+  orderdf.show()
+
+}
